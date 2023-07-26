d2f128b266ca9ebf3af4f9c973c3b5762d0eec2b
PCC <105041457+pcchristie@users.noreply.github.com>
2023-07-26 14:16:22 +0800

    Updated to add random club, time and evaluation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const responses = ['shit', 'vanilla', 'strong', 'flag bound'];
+const evaluations = ['shit', 'vanilla', 'strong', 'flag bound'];
+const clubs = ['Pies', 'Dons', 'Dockers', 'Eagles', 'Blues'];
+const timeline = ['next year', 'in a few years', 'until the heat death of the universe']
 
-const footyFortuneTeller = (club) => {
-    let rand = Math.floor(Math.random() * 4);
-    console.log(`Here's what I think about ${club}... they are ${responses[rand]}`);
+const footyFortuneTeller = () => {
+    let randEval = Math.floor(Math.random() * evaluations.length);
+    let randClub = Math.floor(Math.random() * clubs.length);
+    let randTime = Math.floor(Math.random() * timeline.length);
+    console.log(`I can't tell you much, but I can give you one prediction with absolute certainty, I can tell you that ${timeline[randTime]}, the ${clubs[randClub]} will be ${evaluations[randEval]}`);
 }
-
-console.log(responses);
 
 footyFortuneTeller('Essendon');
